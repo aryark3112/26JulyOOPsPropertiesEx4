5e5b9a79a8edf1ca319e3acec26ed30e26b24075
aryark <aryark3112@gmail.com>
2023-07-26 11:07:06 +0530

    first commit

---
+++ Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesEx4
+{
+    public class Customer
+    {
+        private int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value >= 1)
+                { id = value; }
+                else { id = -1; }
+            }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length > 6)
+                { name = value; }
+                else
+                {
+                    name = "invalid";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Console.WriteLine("Enter Id");
+            customer.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            customer.Name = Console.ReadLine();
+            Console.WriteLine("Customer Details");
+            Console.WriteLine("ID: "+customer.Id+"\n Name: "+customer.Name);
+            Console.ReadKey();
+
+        }
+    }
+}
